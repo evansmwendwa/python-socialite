3ae4b06323afcea9afab562241906739e58c3d59
Evans Mwendwa <evans.mwendwa@live.com>
2020-05-10 20:07:20 +0300

    lint

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import codecs
 import os.path
 from setuptools import setup, find_packages  # type: ignore
 
+
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
+
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
